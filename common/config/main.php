91ac46ee51b9617715b542a59e8e8f4f9450e0fb
thomas <webhoppe@web.de>
2015-04-25 15:33:06 +0700

    after migrate

--- common/config/main.php
+++ common/config/main.php
@@ -1,7 +1,39 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
     'language' => 'en',
+    'modules' => [
+        'social' => [
+            // the module class
+            'class' => 'kartik\social\Module',
+            // the global settings for the disqus widget
+            'disqus' => [
+                'settings' => ['shortname' => 'DISQUS_SHORTNAME'] // default settings
+            ],
+            // the global settings for the facebook plugins widget
+            'facebook' => [
+                'appId' => '1435496006746447',
+                'secret' => 'fe214b605d4d422f04a9f010022176fe',
+            ],
+            // the global settings for the google plugins widget
+            'google' => [
+                'clientId' => 'GOOGLE_API_CLIENT_ID',
+                'pageId' => 'GOOGLE_PLUS_PAGE_ID',
+                'profileId' => 'GOOGLE_PLUS_PROFILE_ID',
+            ],
+            // the global settings for the google analytic plugin widget
+            'googleAnalytics' => [
+                'id' => 'TRACKING_ID',
+                'domain' => 'TRACKING_DOMAIN',
+            ],
+            // the global settings for the twitter plugin widget
+            'twitter' => [
+                'screenName' => 'TWITTER_SCREEN_NAME'
+            ],
+        ],
+        // your other modules
+    ],
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -20,3 +52,4 @@ return [
         ],
     ],
 ];
+
